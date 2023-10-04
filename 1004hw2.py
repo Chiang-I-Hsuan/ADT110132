3647ba2ae457e77a74c11b93f796b4276a72cede
Chiang-I-Hsuan <102507767+Chiang-I-Hsuan@users.noreply.github.com>
2023-10-04 16:01:45 +0800

    Add files via upload

---
+++ 1004hw2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range (1,10):
+    #print("i=",i)
+    for j in range(1,10):
+        #print("j=",j)
+        print(i,"*",j,"=",i*j,end="  ")
+    
